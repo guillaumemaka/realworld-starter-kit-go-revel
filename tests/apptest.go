cc13d8bee7fcbb8cda8759d61ae9fdda4ef8bb6d
Guillaume Maka <guillaume.maka@gmail.com>
2017-05-26 01:35:03 -0400

    Setup Test

--- tests/apptest.go
+++ tests/apptest.go
@@ -1,23 +1,59 @@
 package tests
 
 import (
+	"github.com/guillaumemaka/realworld-starter-kit-go-revel/app/lib/auth"
+	"github.com/guillaumemaka/realworld-starter-kit-go-revel/app/models"
+	"github.com/jinzhu/gorm"
+	"github.com/revel/revel"
 	"github.com/revel/revel/testing"
+	"gopkg.in/testfixtures.v2"
 )
 
 type AppTest struct {
 	testing.TestSuite
 }
 
-func (t *AppTest) Before() {
-	println("Set up")
-}
+var (
+	DB       *gorm.DB
+	fixtures *testfixtures.Context
+	JWT      auth.Tokener
+	users    []models.User
+)
 
-func (t *AppTest) TestThatIndexPageWorks() {
-	t.Get("/")
-	t.AssertOk()
-	t.AssertContentType("text/html; charset=utf-8")
+func (t *AppTest) Before() {
+	dialect, _ := revel.Config.String("db.dialect")
+	dbname, _ := revel.Config.String("db.dbname")
+
+	db, err := models.NewDB(dialect, dbname)
+
+	if err != nil {
+		revel.ERROR.Fatal(err)
+	}
+
+	DB = db.DB
+	JWT = auth.NewJWT()
+
+	fixtures, err = testfixtures.NewFolder(DB.DB(), &testfixtures.SQLite{}, "tests/testdata/fixtures")
+
+	if err != nil {
+		revel.ERROR.Fatal(err)
+	}
+
+	err = fixtures.Load()
+
+	if err != nil {
+		revel.ERROR.Fatal(err)
+	}
+
+	DB.Find(&users)
 }
 
 func (t *AppTest) After() {
 	println("Tear down")
+}
+
+func (t *AppTest) TestConnection() {
+	t.Assert(DB != nil)
+	t.Assert(fixtures != nil)
+	t.AssertEqual(8, len(users))
 }
