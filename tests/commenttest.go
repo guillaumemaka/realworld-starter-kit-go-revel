53c2598b2fde24e9a7bc3e48d7ad67118dcbb8b3
Guillaume Maka <guillaume.maka@gmail.com>
2017-05-28 01:26:00 -0400

    add test file [skip ci]

---
+++ tests/commenttest.go
@@ -0,0 +1,29 @@
+package tests
+
+type CommentController struct {
+	AppTest
+}
+
+func (t *CommentController) TestGetComments() {
+
+}
+
+func (t *CommentController) TestGetComment() {
+
+}
+
+func (t *CommentController) TestCreateComment() {
+
+}
+
+func (t *CommentController) TestCreateCommentUnauthorized() {
+
+}
+
+func (t *CommentController) TestDeleteComment() {
+
+}
+
+func (t *CommentController) TestDeleteCommentUnauthorized() {
+
+}
